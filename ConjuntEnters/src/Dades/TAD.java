c1fd79ecfdca8df04efd0a20a43e577f57df3ca0
AleixMT <aleixaretra@gmail.com>
2017-02-27 10:25:13 +0100

    Programant la primera implementació, a la espera de consulta amb professor de l'assignatura. Es pot continuar fent el main, amb el que es demana, afegint els menús i les diferents excepcions. Deixarem la interfície pel final. Tot això ha d'estar fet pel dijous, i a partir d'allí programar les altres tres estructures serà ràpid

---
+++ ConjuntEnters/src/Dades/TAD.java
@@ -0,0 +1,45 @@
+package Dades;
+
+public abstract class TAD implements TADinterface {
+	protected char[] Msg;
+	protected int[] key;
+	protected int numElem;
+	protected int numKeys;
+	/**
+	 * La part comú de codi que tenen les tres implementacions son un apuntador i una taula de 
+	 * caracters. També tots tindran accés a una clau
+	 * 
+	 * @param num
+	 */
+	public TAD(int num, int[] key) {
+		this.Msg=new char[num];
+		this.key=key;
+		this.numElem=0;
+		this.numKeys=0;
+	}
+	
+	public int getnumElem(){
+		return this.numElem;
+	}
+	public int[] getKey(){
+		return(this.key);
+	}
+	
+	/**
+	 * Aquest métode hauria d'anar desactivat a la versió definitiva (privat)
+	 * Retorna l'element del missatge passat per parametre
+	 * @param num
+	 * @return
+	 */
+	public char[] getMsg(){
+		return this.Msg;
+	}
+	
+	public int[] getkey(){
+		return this.key;
+	}
+	
+	public boolean ple(){
+		return (this.numElem==this.Msg.length);
+	}
+}
