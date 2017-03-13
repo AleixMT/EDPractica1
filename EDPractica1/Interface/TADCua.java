b9dce1eb971e0fdbcb5d246e5ea6d99643992793
crizloz <crizloz@gmail.com>
2017-03-13 21:28:01 +0100

    format pre/post del TAD

--- EDPractica1/Interface/TADCua.java
+++ EDPractica1/Interface/TADCua.java
@@ -9,6 +9,8 @@ import Exceptions.*;
 public abstract interface TADCua {
 	/**
 	 * Afegeix un enter de la clau a la cua
+	 * @pre cert
+	 * @post es igual a la cua inicial pero amb el enter e afegit
 	 * @param e - enter a afegir
 	 * @throws CuaPlena - la cua esta plena i no es pot afegir el valor
 	 */
@@ -16,6 +18,8 @@ public abstract interface TADCua {
 	
 	/**
 	 * Retorna i elimina el primer de la cua
+	 * @pre cert
+	 * @post igual a la cua inicial pero sense el primer enter que hi havia guardat
 	 * @return l'enter que hi ha el primer de la cua
 	 * @throws CuaBuida - la cua esta buida i no hi ha cap enter per a retornar
 	 */
@@ -23,6 +27,8 @@ public abstract interface TADCua {
 	
 	/**
 	 * Retorna el primer de la cua
+	 * @pre cert
+	 * @post la cua es igual a la inicial
 	 * @return l'enter que hi ha el primer de la cua
 	 * @throws CuaBuida - la cua esta buida i no hi ha cap enter per a retornar
 	 */
@@ -30,18 +36,24 @@ public abstract interface TADCua {
 	
 	/**
 	 * Retorna si la cua esta buida
+	 * @pre cert
+	 * @post la cua es igual a la cua inicial
 	 * @return cert si la cua esta buida, fals en cas contrari
 	 */
 	public boolean esBuida();
 	
 	/**
 	 * Retorna el numero d'enters guardats a la cua
+	 * @pre cert
+	 * @post la cua es igual a la inicial
 	 * @return numero d'enters guardats a la cua
 	 */
 	public int numElem();
 	
 	/**
 	 * Retorna si la cua esta plena
+	 * @pre cert
+	 * @post la cua es igual a la inicial
 	 * @return cert si la cua esta plena, fals en cas contrari
 	 */
 	public boolean esPlena();
